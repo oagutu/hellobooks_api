649549d547176435401868109b1cfb7ae96b62b3
oagutu <oobola@yahoo.com>
2018-03-27 13:28:25 +0300

    setup flask evironment

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask_api import FlaskAPI
+
+from config import app_config
+
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    
+
+    return app
