ac5d5c03e3fb0da27d4ce4b42c8fa0fa7c4d535e
oagutu <oobola@yahoo.com>
2018-04-04 13:32:50 +0300

    separate test cases[#156496180]

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
-'''
+"""
     tests/test_users.py
-    Provides testing for book endpoints 
-'''
+    Provides testing for book endpoints.
+"""
 
 import unittest
 import json
@@ -10,7 +10,9 @@ from app import create_app
 
 
 class UserEndpointsTestCase(unittest.TestCase):
-    '''Tests book API endpoints'''
+    """
+    Tests user API endpoints."""
+
 
     def setUp(self):
         '''sets up testing environment variables'''
@@ -18,53 +20,69 @@ class UserEndpointsTestCase(unittest.TestCase):
         self.app = create_app('development')
         self.client = self.app.test_client()
         self.user_details ={
-                "name": "Jane Doe",
-                "user_id": 123456,
-                "username": "JD",
-                "password": "qwerty",
-                "email": "abc@gfg.com",
-                "acc_status": "member",
-                "borrowed_books": {}
+            "name" : "Jane Doe",
+            "user_id" : 123456,
+            "username" : "JD",
+            "password" : "qwerty",
+            "email" : "abc@gfg.com",
+            "acc_status" : "member",
+            "borrowed_books" : {}
         }
 
         self.user_details_two = {
-            'name': 'Baba',
-            'user_id': '1234',
-            'username': 'John',
-            'password': 'qwerty',
-            'email': 'qwerty@keyboard.com',
-            'acc_status': 'suspended',
-            'borrowed_books': {}}
+            "name" : "Baba'",
+            "user_id" : "1234",
+            "username" : "John",
+            "password" : "qwerty",
+            "email" : "qwerty@keyboard.com",
+            "acc_status" : "suspended",
+            "borrowed_books" : {}}
+
+        self.client.post(
+            "/api/v1/auth/login",
+            data=json.dumps({'username': 'JD', 'password': 'qwerty'}),
+            headers={"content-type": "application/json"})
 
 
     def test_create_user_account(self):
-        '''tests create_user_account functionality'''
+        """
+        Tests create_user_account functionality."""
 
         result = self.client.post(
             "/api/v1/auth/register",
             data=json.dumps(self.user_details),
-            headers={"content-type": "application/json"})
+            headers={"content-type" : "application/json"})
         print(result.data)
         self.assertEqual(result.status_code, 201)
         self.assertIn(b'JD', result.data)
         self.assertIn(b'123456', result.data)
         
-        #test case: user name already in use
+
+    def test_create_user_account_with_conflict(self):
+        """
+        Tests if username already in use when creating an account"""
+
         result = self.client.post(
             "/api/v1/auth/register",
             data=json.dumps(self.user_details_two),
-            headers={"content-type": "application/json"})
+            headers={"content-type" : "application/json"})
         self.assertEqual(result.status_code, 409)
         self.assertIn(b'Username not available. Already in use', result.data)
 
-    def test_login_reset_logout(self):
-        '''tests login(), reset_password() and logout() functionality'''
+    def test_login(self):
+        """
+        Tests login(), reset_password() and logout() functionality."""
 
         result = self.client.post(
             "/api/v1/auth/login",
             data=json.dumps({'username': 'JD', 'password': 'qwerty'}),
             headers={"content-type": "application/json"})
         self.assertIn(b'Successfully logged in', result.data)
+
+    def test_login_incorrect_credentials(self):
+        """
+        Tests login with wrong username/password"""
+
         result = self.client.post(
             "/api/v1/auth/login",
             data=json.dumps({'username': 'JD', 'password': 'rose'}),
@@ -76,20 +94,28 @@ class UserEndpointsTestCase(unittest.TestCase):
             headers={"content-type": "application/json"})
         self.assertIn(b'Account not available', result.data)
 
-        #password reset test
+    def test_reset_password(self):
+        """
+        Tests password reset"""
+
         result = self.client.post(
             'api/v1/auth/reset-password',
-            data=json.dumps({'username': 'JD', 'current_password': 'qwerty', 'new_password' : "09876"}),
+            data=json.dumps({'username': 'JD', 'current_password': 'qwerty', 'new_password' : '09876'}),
             headers={"content-type": "application/json"})
         self.assertEqual(result.status_code, 202)
         self.assertIn(b'Successfully changed password', result.data)
-        result=self.client.post(
+        
+        result = self.client.post(
             'api/v1/auth/reset-password',
-            data = json.dumps({'username': 'JD', 'current_password': 'pass123', 'new_password': "09876"}),
-            headers = {"content-type": "application/json"})
+            data=json.dumps({'username' : 'JD', 'current_password' : 'pass123', 'new_password' : '09876'}),
+            headers={"content-type": "application/json"})
         self.assertIn(b'Current password incorrect', result.data)
 
-        #logout test
+
+    def test_logout(self):
+        """
+        Tests logout"""
+
         result = self.client.post(
             'api/v1/auth/logout')
         self.assertIn(b'Successfully logged out', result.data)
