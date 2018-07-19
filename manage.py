92716a1df3807297cb879b3237509aba11cb785a
oagutu <agutu@obolas-MacBook-Pro.local>
2018-07-19 11:44:18 +0300

    ch restructure migrations

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 """
 Manage database migrations.
 """
-from flask_script import Manager # class for handling a set of commands
+from flask_script import Manager  # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from app.users import models
